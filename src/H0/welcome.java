8ac7fdc63205893369189134b8af74ec8aa41347
AdmiN <AdmiN@ITWS0039>
2024-08-20 10:17:22 +0530

    commit

---
+++ src/H0/welcome.java
@@ -0,0 +1,10 @@
+package H0;
+
+public class welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          System.out.println("Welcome to devops");
+	}
+
+}
